9e9e26e4b9c1d42b64809e6e4a3d81b979450cd6
Alexis Berthelot <berthelota2@gmail.com>
2024-08-05 12:31:50 +0200

    enumOperator

--- src/sample/guiform/example/Calculator.java
+++ src/sample/guiform/example/Calculator.java
@@ -3,7 +3,7 @@ package sample.guiform.example;
 import javax.swing.*;
 
 public class Calculator {
-    private JTextField results;
+    private JTextField resultsTxt;
     private JPanel calculatorView;
     private JButton clearBtn;
     private JButton signBtn;
@@ -13,4 +13,26 @@ public class Calculator {
     private JButton eightBtn;
     private JButton nineBtn;
     private JButton multiplyBtn;
+    private JButton fourBtn;
+    private JButton fiveBtn;
+    private JButton sixBtn;
+    private JButton minusBtn;
+    private JButton oneBtn;
+    private JButton twoBtn;
+    private JButton threeBtn;
+    private JButton addBtn;
+    private JButton zeroBtn;
+    private JButton digitBtn;
+    private JButton equalBtn;
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Calculator");
+        frame.setContentPane(new Calculator().calculatorView);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
+
+
