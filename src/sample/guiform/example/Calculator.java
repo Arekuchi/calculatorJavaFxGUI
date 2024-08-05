4716911840327c39e1c0008e98bd5738208b1587
Alexis Berthelot <berthelota2@gmail.com>
2024-08-05 12:20:21 +0200

    first commit

---
+++ src/sample/guiform/example/Calculator.java
@@ -0,0 +1,16 @@
+package sample.guiform.example;
+
+import javax.swing.*;
+
+public class Calculator {
+    private JTextField results;
+    private JPanel calculatorView;
+    private JButton clearBtn;
+    private JButton signBtn;
+    private JButton percentBtn;
+    private JButton divideBtn;
+    private JButton sevenBtn;
+    private JButton eightBtn;
+    private JButton nineBtn;
+    private JButton multiplyBtn;
+}
